6328bde186e829ba47f0be07602909100f844256
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-03-25 18:09:56 +0000

    feat: add `IndexedWeakData` and `IndexedNamedWeakData` for objects.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,6 +3,8 @@ export { DataCore } from './data-core.abstract';
 export { Immutability } from './immutability.abstract';
 // Class.
 export { Data } from './data.class';
+export { IndexedNamedWeakData } from './indexed-named-weak-data.class';
+export { IndexedWeakData } from './indexed-weak-data.class';
 export { NamedWeakData } from './named-weak-data.class';
 export { Value } from './value.class';
 export { WeakData } from './weak-data.class';
