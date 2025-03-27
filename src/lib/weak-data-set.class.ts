a983646aa568bf303e24ad1df1ce949540ccdf1d
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-03-27 19:03:13 +0000

    feat(WeakDataSet): add the `Set` that uses `WeakData` store mechanism.

---
+++ src/lib/weak-data-set.class.ts
@@ -0,0 +1,30 @@
+// Class.
+import { DataSet } from './data-set.class';
+import { WeakData } from './weak-data.class';
+/**
+ * @description
+ * @export
+ * @class WeakDataSet
+ * @template Type 
+ * @extends {DataSet<Type, WeakData<Set<Type>>>}
+ */
+export class WeakDataSet<Type> extends DataSet<Type, WeakData<Set<Type>>> {
+  /**
+   * @description Returns the `string` tag representation of the `WeakDataSet` class when used in `Object.prototype.toString.call(instance)`.
+   * @public
+   * @readonly
+   * @type {string}
+   */
+  public override get [Symbol.toStringTag](): string {
+    return WeakDataSet.name;
+  }
+
+  /**
+   * Creates an instance of `WeakDataSet`.
+   * @constructor
+   * @param {?(Iterable<Type> | null | undefined)} [iterable] 
+   */
+  constructor(iterable?: Iterable<Type> | null | undefined) {
+    super(iterable, WeakData);
+  }
+}
