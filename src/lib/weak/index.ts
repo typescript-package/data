6b774e4e9084fcdf5dc55f6902196c355e0ca2c1
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-04-05 18:12:42 +0000

    chore(weak): update.

--- src/lib/weak/index.ts
+++ src/lib/weak/index.ts
@@ -1,4 +1,3 @@
-export { IndexedNamedWeakData  } from './indexed-named-weak-data.class';
 export { IndexedWeakData } from './indexed-weak-data.class';
 export { NamedWeakData } from './named-weak-data.class';
 export { WeakData } from './weak-data.class';
