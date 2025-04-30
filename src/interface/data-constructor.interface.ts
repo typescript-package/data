380c0a0f11c103726e1f2c9482a34c92b5fa881b
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-04-30 18:22:11 +0000

    refactor(`DataConstructor`): add `Args`.

--- src/interface/data-constructor.interface.ts
+++ src/interface/data-constructor.interface.ts
@@ -1,8 +1,16 @@
 import { DataCore } from '../lib';
-
+/**
+ * @description
+ * @export
+ * @interface DataConstructor
+ * @template Value 
+ * @template {DataCore<Value>} DataType 
+ * @template {readonly any[]} Args 
+ */
 export interface DataConstructor<
   Value,
-  DataType extends DataCore<Value>
+  DataType extends DataCore<Value>,
+  Args extends readonly any[] = any[]
 >  {
-  new (value: Value): DataType;
+  new (value: Value, ...args: Args): DataType;
 }
