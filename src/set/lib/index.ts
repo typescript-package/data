d856ec01517d0bd88d49f959eddc1e66497bb963
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-01 16:09:56 +0000

    chore(set): update index.

--- src/set/lib/index.ts
+++ src/set/lib/index.ts
@@ -1,5 +1,6 @@
-
+// Abstract.
 export { CoreSet } from './core-set.abstract';
+export { SetOnHook } from './set-on-hook.abstract';
+// Class.
 export { DataSet } from './data-set.class';
 export { ImmutableSet } from './immutable-set.class';
-export { SetOnHook } from './set-on-hook.abstract';
