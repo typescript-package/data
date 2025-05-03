c4c57a3b4f22cc9bc0eede3d10a5aa9b8bc5a3fa
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-03 21:25:50 +0000

    chore(api): update.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,6 +3,8 @@ export { DataCore } from './data-core.abstract';
 export { Immutability } from './immutability.abstract';
 // Class.
 export { Data } from './data.class';
+export { ImmutableData } from './immutable-data.class';
+export { ReadonlyData } from './readonly-data.class';
 // `WeakData`.
 export { IndexedWeakData } from './indexed-weak-data.class';
 export { NamedWeakData } from './named-weak-data.class';
