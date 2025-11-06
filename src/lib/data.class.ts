7cd6cca2b38d7184821f6d5c899ed75f2276e8d5
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-11-06 22:01:07 +0000

    reactor(`Data`): use `ValueShape` and `DataBase`.

--- src/lib/data.class.ts
+++ src/lib/data.class.ts
@@ -1,88 +1,39 @@
 // Abstract.
-import { DataCore } from './data-core.abstract';
-// Class.
-import { Value } from './value.class';
+import { DataBase } from './data-base.abstract';
+// Interface.
+import { ValueShape } from '@typedly/data';
 /**
  * @description The `Data` class is a concrete class that wraps a value and provides methods for setting, retrieving, and destroying the value.
  * @export
  * @class Data
- * @template Type 
- * @extends {DataCore<Type>}
+ * @template T 
+ * @template {ValueShape<T>} [I=ValueShape<T>] 
+ * @template {unknown[]} [V=unknown[]] 
+ * @extends {DataCore<T>}
  */
-export class Data<Type> extends DataCore<Type> {
+export class Data<
+  // Type of the data value.
+  T,
+  // Value instance type extending ValueShape<Type>.
+  I extends ValueShape<T> = ValueShape<T>,
+  // Arguments for the value constructor.
+  V extends unknown[] = unknown[],
+> extends DataBase<T, I, V> {
   /**
-   * @description Returns the `string` tag representation of the `Data` class when used in `Object.prototype.toString.call(instance)`.
+   * @inheritdoc
+   * @public
+   * @readonly
+   * @type {string}
+   */
+  public static override toStringTag: string = 'Data';
+
+  /**
+   * @inheritdoc
    * @public
    * @readonly
    * @type {string}
    */
   public override get [Symbol.toStringTag](): string {
-    return Data.name;
-  }
-  
-  /**
-   * @description Returns the privately stored value of generic type variable `Type`.
-   * @public
-   * @readonly
-   * @type {Type}
-   */
-  public get value(): Type {
-    return this.#value.value;
-  }
-
-  /**
-   * @description Privately stored value of class `Value<Type>`.
-   * @type {Value<Type>}
-   */
-  #value: Value<Type>;
-
-  /**
-   * Creates an instance of `Data`.
-   * @constructor
-   * @param {Type} value Initial data value of generic type variable `Type`.
-   */
-  constructor(value: Type) {
-    super();
-    this.#value = new Value(value);
-  }
-
-  /**
-   * @description 
-   * @public
-   * @returns {Type} 
-   */
-  public [Symbol.for('value')](): Type {
-    return this.value;
-  }
-
-  /**
-   * @description Clears the value to `null`.
-   * @public
-   * @returns {this} The `this` current instance.
-   */
-  public clear(): this {
-    this.#value.set(null as unknown as Type);
-    return this;
-  }
-
-  /**
-   * @description Destroys the `Value` object by setting it to `null`.
-   * @public
-   * @returns {this} The `this` current instance.
-   */
-  public destroy(): this {
-    this.#value = null as any;
-    return this;
-  }
-
-  /**
-   * @description Sets the data value.
-   * @public
-   * @param {Type} value The data value of `Type` to set.
-   * @returns {this} The `this` current instance.
-   */
-  public set(value: Type): this {
-    super.validate(), this.#value!.set(value);
-    return this;
+    return Data.toStringTag;
   }
 }
