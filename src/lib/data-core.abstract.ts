7186d7379ae05bfdb6dad4c3a9486cd207c267c8
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-11-06 21:08:02 +0000

    refactor(`DataCore`): use the static toStringTag, add static `isEqual()` for static `hasChanged()` default behavior that is used in the onChange hook. handle the `onChange` `onDestroy` `onSet` callbacks, change the `Type` to `T`.

--- src/lib/data-core.abstract.ts
+++ src/lib/data-core.abstract.ts
@@ -7,10 +7,52 @@ import { DataShape } from '@typedly/data';
  * @export
  * @abstract
  * @class DataCore
- * @template Type Represents the type of data value.
+ * @template T Represents the type of data value.
  * @extends {Immutability}
+ * @implements {DataShape<T>}
  */
-export abstract class DataCore<Type> extends Immutability implements DataShape<Type> {
+export abstract class DataCore<T>
+  // For immutability features.
+  extends Immutability
+  // For data shape contract, to use instead of `DataCore`.
+  implements DataShape<T> {
+  /**
+   * @description Symbol key for accessing the value of the data instance.
+   * @public
+   * @static 
+   * @type {*}
+   */
+  public static valueSymbol = Symbol.for('value');
+
+  /**
+   * @description The `string` tag representation of the `DataCore` class when used in `Object.prototype.toString.call(instance)`.
+   * @public
+   * @static
+   * @type {string}
+   */
+  public static toStringTag = 'DataCore';
+
+  /**
+   * @description Determine whether two values are different used in the `onChange` hook.
+   * @param {T} a The first value to compare.
+   * @param {T} b The second value to compare.
+   * @returns {boolean} The comparison result of `boolean` type.
+   */
+  public static hasChanged: <T>(a: T, b: T) => boolean = <T>(a: T, b: T): boolean => !DataCore.isEqual(a, b);
+
+  /**
+   * @description Checks if two values are different.
+   * @public
+   * @static
+   * @template T 
+   * @param {T} a The first value to compare.
+   * @param {T} b The second value to compare.
+   * @returns {boolean} The comparison result of `boolean` type.
+   */
+  public static isEqual<T>(a: T, b: T): boolean {
+    return JSON.stringify(a) === JSON.stringify(b);
+  }
+
   /**
    * @description Returns the `string` tag representation of the `DataCore` class when used in `Object.prototype.toString.call(instance)`.
    * @public
@@ -18,7 +60,7 @@ export abstract class DataCore<Type> extends Immutability implements DataShape<T
    * @type {string}
    */
   public get [Symbol.toStringTag](): string {
-    return DataCore.name;
+    return DataCore.toStringTag;
   }
 
   /**
@@ -32,13 +74,61 @@ export abstract class DataCore<Type> extends Immutability implements DataShape<T
   }
 
   /**
-   * @description Returns the value of generic type variable `Type`.
+   * @description Returns the value of generic type variable `T`.
    * @public
    * @abstract
    * @readonly
-   * @type {Type}
+   * @type {T}
    */
-  public abstract get value(): Type;
+  public abstract get value(): T;
+
+  /**
+   * @description Returns the onChange callback function.
+   * @protected
+   * @readonly
+   * @type {((value: T, oldValue: T) => T) | undefined}
+   */
+  protected get onChangeCallback(): ((value: T, oldValue: T) => T) | undefined {
+    return this.#onChangeCallback;
+  }
+
+  /**
+   * @description Returns the onDestroy callback function.
+   * @protected
+   * @readonly
+   * @type {(() => void) | undefined}
+   */
+  protected get onDestroyCallback(): (() => void) | undefined {
+    return this.#onDestroyCallback;
+  }
+
+  /**
+   * @description Returns the onSet callback function.
+   * @protected
+   * @readonly
+   * @type {((value: T) => T) | undefined}
+   */
+  protected get onSetCallback(): ((value: T) => T) | undefined {
+    return this.#onSetCallback;
+  }
+
+  /**
+   * @description Privately stored onChange callback function, defaults `undefined`.
+   * @type {?(value: T, oldValue: T) => T}
+   */
+  #onChangeCallback?: (value: T, oldValue: T) => T;
+
+  /**
+   * @description Privately stored onDestroy callback function, defaults `undefined`.
+   * @type {?() => void}
+   */
+  #onDestroyCallback?: () => void;
+
+  /**
+   * @description Privately stored onSet callback function, defaults `undefined`.
+   * @type {?(value: T) => T}
+   */
+  #onSetCallback?: (value: T) => T;
 
   /**
    * @description Clears the value by setting to `undefined` or `null`.
@@ -62,17 +152,49 @@ export abstract class DataCore<Type> extends Immutability implements DataShape<T
    * @returns {this} 
    */
   public override lock(): this {
-    Immutability.deepFreeze(this.value);
-    super.lock(); 
-    return this;
+    return Immutability.deepFreeze(this.value),
+      super.lock(),
+      this;
+  }
+
+  /**
+   * @description Sets the callback function invoked when the data value changes.
+   * @public
+   * @abstract
+   * @param {(value: T, oldValue: T) => T} callbackfn The callback function to invoke.
+   * @returns {this} The `this` current instance.
+   */
+  public onChange(callbackfn: (value: T, oldValue: T) => T): this {
+    return this.#onChangeCallback = callbackfn, this;
+  }
+
+  /**
+   * @description Sets the callback function to be invoked when destroying the data instance.
+   * @public
+   * @param {() => void} callbackfn 
+   * @returns {this} 
+   */
+  public onDestroy(callbackfn: () => void): this {
+    return this.#onDestroyCallback = callbackfn, this;
+  }
+
+  /**
+   * @description Sets the callback function to be invoked when setting the data value.
+   * @public
+   * @abstract
+   * @param {(value: T) => T} callbackfn The callback function to invoke.
+   * @returns {this} The `this` current instance.
+   */
+  public onSet(callbackfn: (value: T) => T): this {
+    return this.#onSetCallback = callbackfn, this;
   }
 
   /**
    * @description Sets the data value. Ensure `super.validate()` is called before invoking this method.
    * @public
    * @abstract
-   * @param {Type} value The data value of `Type` to set.
+   * @param {T} value The data value of `T` to set.
    * @returns {this} Returns `this` current instance.
    */  
-  public abstract set(value: Type): this;
+  public abstract set(value: T): this;
 }
