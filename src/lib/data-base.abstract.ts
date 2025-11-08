abf4329c721c820be8543ae4fd5ede1c74ff1c08
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-11-08 02:32:47 +0000

    refactor(`DataBase`): move to the experimental.

--- src/lib/data-base.abstract.ts
+++ src/lib/data-base.abstract.ts
@@ -1,25 +1,13 @@
 // Abstract.
 import { DataCore } from './data-core.abstract';
-// Interface.
-import { ValueConstructor, ValueShape } from '@typedly/data';
 /**
  * @description The `Data` class is a concrete class that wraps a value and provides methods for setting, retrieving, and destroying the value.
  * @export
  * @class Data
- * @template T 
- * @template {ValueShape<T>} [I=ValueShape<T>] 
- * @template {unknown[]} [V=unknown[]] 
- * @template {unknown[]} [D=unknown[]] 
+ * @template T Type of the data value.
  * @extends {DataCore<T>}
  */
-export abstract class DataBase<
-  // Type of the data value.
-  T,
-  // Value instance type extending ValueShape<Type>.
-  I extends ValueShape<T> = ValueShape<T>,
-  // Arguments for the value constructor.
-  V extends unknown[] = unknown[],
-> extends DataCore<T> {
+export abstract class DataBase<T> extends DataCore<T> {
   /**
    * @description Returns the `string` tag representation of the `Data` class when used in `Object.prototype.toString.call(instance)`.
    * @public
@@ -31,81 +19,29 @@ export abstract class DataBase<
   }
 
   /**
-   * @description Returns the value of the data instance.
-   * @public
-   * @returns {I | undefined} The value of the data instance.
-   */
-  public get [DataCore.valueSymbol](): I | undefined {
-    return this.#valueCtor ? this.#value as I : undefined;
-  }
-
-  /**
-   * @description Returns the privately stored value of generic type variable `Type`.
+   * @description Returns the privately stored value of generic type variable `T`.
    * @public
    * @readonly
    * @type {T}
    */
   public get value(): T {
-    return this.#valueCtor
-      ? (this.#value as I).value
-      : this.#value as T;
+    return this.#value;
   }
 
   /**
-   * @description Privately stored value of class `ValueShape<Type>`.
-   * @type {T | I}
+   * @description Privately stored value of class `T`.
+   * @type {T}
    */
-  #value: T | I;
-
-  /**
-   * @description Privately stored value to determine value constructor was used.
-   * @type {boolean | undefined}
-   */
-  #valueCtor;
+  #value: T;
 
   /**
    * Creates an instance of `DataBase`.
    * @constructor
-   * @param {T} value Initial data value of generic type variable `Type`.
-   * @param {?[ValueConstructor<T, I>, ...V]} [valueCtor] 
+   * @param {T} value Initial data value of generic type variable `T`.
    */
-  constructor(
-    value: T,
-    valueCtor?: [ValueConstructor<T, I>, ...V],
-  );
-
-  /**
-   * Creates an instance of `DataBase`.
-   * @constructor
-   * @param {T} value Initial data value of generic type variable `Type`.
-   * @param {?ValueConstructor<T, I>} [valueCtor] The value constructor.
-   */
-  constructor(
-    value: T,
-    valueCtor?: ValueConstructor<T, I>,
-  );
-
-  /**
-   * Creates an instance of `DataBase`.
-   * @constructor
-   * @param {T} value Initial data value of generic type variable `Type`.
-   * @param {unknown[]} args The rest of the arguments.
-   */
-  constructor(value: T, ...args: unknown[]) {
+  constructor(value: T) {
     super();
-    //  Assign value based on whether a value constructor is provided.
-    const valueCtor = args[0] as
-      | ValueConstructor<T, I>
-      | [ValueConstructor<T, I>, ...V ]
-      | undefined;
-    // Set the private value and value constructor flag.
-    this.#valueCtor = valueCtor ? true : undefined;
-    // Initialize the value.
-    this.#value = valueCtor ? (
-      valueCtor instanceof Array
-        ? new valueCtor[0](value, ...valueCtor.slice(1))
-        : new valueCtor(value)
-      ) : value;
+    this.#value = value;
   }
 
   /**
@@ -116,9 +52,7 @@ export abstract class DataBase<
   public clear(): this {
     const oldValue = this.value;
     const newValue = null as unknown as T;
-    return this.#valueCtor
-      ? (this.#value as I).set(null as unknown as T)
-      : this.#value = null as unknown as T,
+    return this.#value = null as unknown as T,
       // Invokes the onChange callback if `newValue` and `this.value` has changed.
       (typeof this.onChangeCallback === 'function' && DataBase.hasChanged(oldValue, newValue)
         ? this.onChangeCallback(newValue, this.value) : newValue),
@@ -142,7 +76,7 @@ export abstract class DataBase<
   /**
    * @description Sets the data value.
    * @public
-   * @param {T} value The data value of `Type` to set.
+   * @param {T} value The data value of `T` to set.
    * @returns {this} The `this` current instance.
    */
   public set(value: T): this {
@@ -152,7 +86,7 @@ export abstract class DataBase<
       // Assigns the new value.
       (newValue = typeof super.onSetCallback === 'function' ? super.onSetCallback(value) : value),
       // Assigns the new value to the private value.
-      (this.#valueCtor ? (this.#value as I).set(newValue) : this.#value = newValue),
+      (this.#value = newValue),
       // Invokes the onChange callback if `newValue` and `this.value` has changed.
       (typeof this.onChangeCallback === 'function' && DataBase.hasChanged(oldValue, newValue)
         ? this.onChangeCallback(newValue, this.value) : newValue),
