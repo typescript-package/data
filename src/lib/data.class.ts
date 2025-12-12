8ad6624c0594afee9213e3d56c087a4c609228d8
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-12-12 01:42:07 +0000

    refactor(`Data`): add `A` generic type variable for `DataAdapter`.

--- src/lib/data.class.ts
+++ src/lib/data.class.ts
@@ -1,13 +1,16 @@
 // Abstract.
-import { DataBase } from './data-base.abstract';
+import { BaseData } from './base-data.abstract';
+// Interface.
+import { DataAdapter } from '@typedly/data';
 /**
- * @description The `Data` class is a concrete class that wraps a value and provides methods for setting, retrieving, and destroying the value.
+ * @description The `Data` class is a concrete class that extends base class with functionality for managing data value.
  * @export
  * @class Data
- * @template T The type of the data value.
- * @extends {DataCore<T>}
+ * @template T Type of the data value.
+ * @template {DataAdapter<T>} [A=DataAdapter<T>] Adapter type extending `DataAdapter` for handling the data value.
+ * @extends {BaseData<T, false, A>} 
  */
-export class Data<T> extends DataBase<T> {
+export class Data<T, A extends DataAdapter<T> = DataAdapter<T>> extends BaseData<T, false, A> {
   /**
    * @inheritdoc
    * @public
