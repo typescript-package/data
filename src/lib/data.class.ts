c958284796bc7fdd89876c9a9eba0299f567cd98
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-12-17 23:23:59 +0000

    refactor(`Data`): update to properly create the concrete class based on `BaseData`.

--- src/lib/data.class.ts
+++ src/lib/data.class.ts
@@ -7,10 +7,17 @@ import { DataAdapter } from '@typedly/data';
  * @export
  * @class Data
  * @template T Type of the data value.
- * @template {DataAdapter<T>} [A=DataAdapter<T>] Adapter type extending `DataAdapter` for handling the data value.
- * @extends {BaseData<T, false, A>} 
+ * @template {unknown[]} [G=unknown[]] Arguments passed to the adapter class constructor, after the `value` parameter.
+ * @template {boolean} [R=false] Indicates whether the data operations are asynchronous.
+ * @template {DataAdapter<T, R> | undefined} [A=DataAdapter<T, R>] Adapter type extending `DataAdapter` for handling the data value.
+ * @extends {BaseData<T, G, R, A>}
  */
-export class Data<T, A extends DataAdapter<T> = DataAdapter<T>> extends BaseData<T, false, A> {
+export class Data<
+  T,
+  G extends unknown[] = unknown[],
+  R extends boolean = false,
+  A extends DataAdapter<T, R> | undefined = DataAdapter<T, R>,
+> extends BaseData<T, G, R, A> {
   /**
    * @inheritdoc
    * @public
