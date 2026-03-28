3efe8cfd04ddf3c97643b024038a50095a592b2a
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-28 22:20:29 +0000

    refactor(`CacheableData`): update type definitions and improve handling of optional settings

--- src/lib/cacheable.data.class.ts
+++ src/lib/cacheable.data.class.ts
@@ -1,20 +1,19 @@
 // Abstract.
 import { ConfigurableData } from './configurable.data.abstract';
 // Interface & Type.
-import type { AsyncReturn, InferAsync } from '@typedly/data';
-import type { CacheableDataSettings } from '../type';
+import type { AsyncReturn, CacheableSettings, DataSettings, InferAsync } from '@typedly/data';
 /**
- * @description The concrete `CacheableData` class extends `ConfigurableData` adding functionality for managing asynchronous data values.
+ * @description The abstract `CacheableData` class extends `ConfigurableData` adding functionality for managing asynchronous data values.
  * Designed to create data containers of `T` type managed by caching mechanisms that may require constructor arguments.
  * @export
  * @class CacheableData
- * @template {CacheableDataSettings<T, R>} C The type of data settings.
+ * @template {DataSettings<R> & CacheableSettings<T>} C The type of data settings.
  * @template T The type of data.
  * @template {boolean} [R=InferAsync<C>] Indicates if the adapter operations are asynchronous.
  * @extends {ConfigurableData<C, T, R>}
  */
-export class CacheableData<
-  const C extends CacheableDataSettings<T, R>,
+export abstract class CacheableData<
+  const C extends DataSettings<R> & CacheableSettings<T>,
   T,
   R extends boolean = InferAsync<C>,
 > extends ConfigurableData<C, T, R> {
@@ -25,7 +24,7 @@ export class CacheableData<
    * @type {string}
    */
   public override get [Symbol.toStringTag](): string {
-    return super.configuration.tag ?? 'CacheableData';
+    return super.configuration?.tag ?? CacheableData.toStringTag;
   }
 
   /**
@@ -47,11 +46,11 @@ export class CacheableData<
   /**
    * Creates an instance of `CacheableData`.
    * @constructor
-   * @param {C} settings Configurable data settings.
+   * @param {?C} [settings] Configurable data settings.
    * @param {?T} [value] Optional initial data value of generic type variable `T`.
    */
   constructor(
-    settings: C,
+    settings?: C,
     value?: T,
   ) {
     super(settings);
@@ -65,7 +64,7 @@ export class CacheableData<
    */
   public clear(): AsyncReturn<R, this> {
     if (super.async) {
-      const { clearer } = super.configuration;
+      const { clearer } = super.configuration ?? {};
       if (!clearer) throw new Error('Asynchronous clearing is not supported without a clearer function in settings.');
       return clearer()?.then(() => (this.#value = undefined as unknown as T, this)) as AsyncReturn<R, this>;
     }
@@ -79,7 +78,7 @@ export class CacheableData<
    */
   public destroy(): AsyncReturn<R, this> {
     if (super.async) {
-      const { destroyer } = super.configuration;
+      const { destroyer } = super.configuration ?? {};
       if (!destroyer) throw new Error('Asynchronous destruction is not supported without a destroyer function in settings.');
       return destroyer()?.then(() => (this.#value = null as unknown as T, this)) as AsyncReturn<R, this>;
     }
@@ -93,7 +92,7 @@ export class CacheableData<
    */
   public getValue(): AsyncReturn<R, T> {
     if (super.async) {
-      const { fetcher } = super.configuration;
+      const { fetcher } = super.configuration ?? {};
       if (!fetcher) throw new Error('Asynchronous value fetching is not supported without a fetcher function in settings.');
       return fetcher() as AsyncReturn<R, T>;
     }
@@ -109,7 +108,7 @@ export class CacheableData<
   public setValue(value: T): AsyncReturn<R, this> {
     super.validate();
     if (super.async) {
-      const { updater } = super.configuration;
+      const { updater } = super.configuration ?? {};
       if (!updater) throw new Error('Asynchronous value setting is not supported without an updater function in settings.');
       return updater(value)?.then(() => (this.#value = value, this)) as AsyncReturn<R, this>;
     }
