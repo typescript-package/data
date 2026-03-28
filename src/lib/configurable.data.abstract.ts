2ff1618257a1cac71ccfea4201d7f0e74db0316d
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-28 22:20:00 +0000

    refactor(`ConfigurableData`): implement `ConfigurableDataShape` interface and adjust type definitions for clarity

--- src/lib/configurable.data.abstract.ts
+++ src/lib/configurable.data.abstract.ts
@@ -1,6 +1,7 @@
 // Abstract.
 import { DataCore } from './data.core';
 // Interface & Type.
+import type { ConfigurableDataShape } from '@typedly/configurable-data';
 import type { DataConfig, DataSettings, InferAsync } from '@typedly/data';
 /**
  * @description The abstract `ConfigurableData` class extends `DataCore` adding functionality for managing value with configuration.
@@ -16,7 +17,8 @@ export abstract class ConfigurableData<
   const C extends DataSettings<R>,
   T,
   R extends boolean = InferAsync<C>,
-> extends DataCore<T, R> {
+> extends DataCore<T, R>
+  implements ConfigurableDataShape<C, T, R> {
   /**
    * @description Returns the `string` tag representation of the `ConfigurableData` class when used in `Object.prototype.toString.call(instance)`.
    * @public
@@ -24,7 +26,7 @@ export abstract class ConfigurableData<
    * @type {string}
    */
   public override get [Symbol.toStringTag](): string {
-    return this.#configuration?.tag ?? 'ConfigurableData';
+    return this.#configuration?.tag ?? ConfigurableData.toStringTag;
   }
 
   /**
@@ -43,22 +45,22 @@ export abstract class ConfigurableData<
    * @readonly
    * @type {DataConfig<C, R>}
    */
-  public get configuration(): DataConfig<C, R> {
+  public get configuration(): DataConfig<C, R> | undefined {
     return this.#configuration;
   }
 
   /**
    * @description Privately stored data configuration of type `DataConfig<C, R>`, which includes the original settings and any additional configuration derived from them.
-   * @type {DataConfig<C, R>}
+   * @type {DataConfig<C, R> | undefined}
    */
-  #configuration: DataConfig<C, R>;
+  #configuration?: DataConfig<C, R>;
 
   /**
    * Creates an instance of parent class.
    * @constructor
-   * @param {C} settings Configurable data settings.
+   * @param {C} [settings] Configurable data settings.
    */
-  constructor(settings: C) {
+  constructor(settings?: C) {
     super();
     this.#configuration = settings as DataConfig<C, R>;
   }
