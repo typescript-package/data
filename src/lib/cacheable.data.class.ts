272a94e9828e18a51ef12859a5dd27def61f4cac
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-05-04 16:58:42 +0000

    refactor(`CacheableData`): update type parameters and improve method implementations for better clarity and functionality

--- src/lib/cacheable.data.class.ts
+++ src/lib/cacheable.data.class.ts
@@ -1,5 +1,5 @@
 // Abstract.
-import { ConfigurableData } from './configurable.data.abstract';
+import { ConfigurableData } from '../configurable/lib/configurable.data.abstract';
 // Interface & Type.
 import type { AsyncReturn, CacheableSettings, DataSettings, InferAsync } from '@typedly/data';
 /**
@@ -7,41 +7,25 @@ import type { AsyncReturn, CacheableSettings, DataSettings, InferAsync } from '@
  * Designed to create data containers of `T` type managed by caching mechanisms that may require constructor arguments.
  * @export
  * @class CacheableData
- * @template {DataSettings<R> & CacheableSettings<T>} C The type of data settings.
+ * @template {DataSettings<S> & CacheableSettings<T>} C The type of data settings.
  * @template T The type of data.
- * @template {boolean} [R=InferAsync<C>] Indicates if the adapter operations are asynchronous.
- * @extends {ConfigurableData<C, T, R>}
+ * @template {boolean} [S=InferAsync<C>] Indicates if the adapter operations are asynchronous.
+ * @extends {ConfigurableData<C, T, S>}
  */
 export abstract class CacheableData<
-  const C extends DataSettings<R> & CacheableSettings<T>,
+  const C extends DataSettings<S> & CacheableSettings<T>,
   T,
-  R extends boolean = InferAsync<C>,
-> extends ConfigurableData<C, T, R> {
+  S extends boolean = InferAsync<C>,
+> extends ConfigurableData<C, T, S> {
   /**
    * @description Returns the `string` tag representation of the `CacheableData` class when used in `Object.prototype.toString.call(instance)`.
    * @public
    * @readonly
    * @type {string}
    */
-  public override get [Symbol.toStringTag](): string {
-    return super.configuration?.tag ?? CacheableData.toStringTag;
+  public override get [Symbol.toStringTag]() {
+    return super.configuration?.tag ? super.configuration.tag : super.tag!;
   }
-
-  /**
-   * @description Returns the privately stored value of generic type variable `T`.
-   * @public
-   * @readonly
-   * @type {T}
-   */
-  public override get value(): T {
-    return this.#value!;
-  }
-
-  /**
-   * @description Privately stored value of type `T`.
-   * @type {T}
-   */
-  #value?: T;
 
   /**
    * Creates an instance of `CacheableData`.
@@ -53,65 +37,68 @@ export abstract class CacheableData<
     settings?: C,
     value?: T,
   ) {
-    super(settings);
-    this.#value = value;
+    super(settings, value);
   }
 
   /**
    * @description Clears the value to `undefined`.
    * @public
-   * @returns {AsyncReturn<R, this>} The `this` current instance.
+   * @returns {AsyncReturn<S, this>} The `this` current instance.
    */
-  public clear(): AsyncReturn<R, this> {
+  public override clear(): AsyncReturn<S, this> {
     if (super.async) {
-      const { clearer } = super.configuration ?? {};
+      const config = this.configuration;
+      const clearer = config && 'clearer' in config && typeof (config as { clearer?: () => Promise<void> }).clearer === 'function' ? (config as { clearer?: () => Promise<void> }).clearer : undefined;
       if (!clearer) throw new Error('Asynchronous clearing is not supported without a clearer function in settings.');
-      return clearer()?.then(() => (this.#value = undefined as unknown as T, this)) as AsyncReturn<R, this>;
+      return clearer()?.then(() => (super.clear(), this)) as AsyncReturn<S, this>;
     }
-    return this.#value = undefined as unknown as T, this as AsyncReturn<R, this>;
+    return super.clear();
   }
 
   /**
    * @description Destroys the value by setting it to `null`.
    * @public
-   * @returns {AsyncReturn<R, this>} The `this` current instance.
+   * @returns {AsyncReturn<S, this>} The `this` current instance.
    */
-  public destroy(): AsyncReturn<R, this> {
+  public override destroy(): AsyncReturn<S, this> {
     if (super.async) {
-      const { destroyer } = super.configuration ?? {};
+      const config = this.configuration;
+      const destroyer = config && 'destroyer' in config && typeof (config as { destroyer?: () => Promise<void> }).destroyer === 'function' ? (config as { destroyer?: () => Promise<void> }).destroyer : undefined;
       if (!destroyer) throw new Error('Asynchronous destruction is not supported without a destroyer function in settings.');
-      return destroyer()?.then(() => (this.#value = null as unknown as T, this)) as AsyncReturn<R, this>;
+      return destroyer()?.then(() => (super.destroy(), this)) as AsyncReturn<S, this>;
     }
-    return (this.#value = null as unknown as T), this as AsyncReturn<R, this>;
+    return super.destroy();
   }
 
   /**
    * @description Gets the value either asynchronously or synchronously based on the `R` generic type variable.
    * @public
-   * @returns {AsyncReturn<R, T>} 
+   * @returns {AsyncReturn<S, T>} 
    */
-  public getValue(): AsyncReturn<R, T> {
+  public override getValue(): AsyncReturn<S, T> {
     if (super.async) {
-      const { fetcher } = super.configuration ?? {};
+      const config = this.configuration;
+      const fetcher = config && 'fetcher' in config && typeof (config as { fetcher?: () => Promise<T> }).fetcher === 'function' ? (config as { fetcher?: () => Promise<T> }).fetcher : undefined;
       if (!fetcher) throw new Error('Asynchronous value fetching is not supported without a fetcher function in settings.');
-      return fetcher() as AsyncReturn<R, T>;
+      return fetcher() as AsyncReturn<S, T>;
     }
-    return this.#value as AsyncReturn<R, T>;
+    return super.getValue();
   }
 
   /**
    * @description Sets the data value.
    * @public
    * @param {T} value The data value of `T` to set.
-   * @returns {AsyncReturn<R, this>} The `this` current instance.
+   * @returns {AsyncReturn<S, this>} The `this` current instance.
    */
-  public setValue(value: T): AsyncReturn<R, this> {
+  public override setValue(value: T): AsyncReturn<S, this> {
     super.validate();
     if (super.async) {
-      const { updater } = super.configuration ?? {};
+      const config = this.configuration;
+      const updater = config && 'updater' in config && typeof (config as { updater?: (value: T) => Promise<void> }).updater === 'function' ? (config as { updater?: (value: T) => Promise<void> }).updater : undefined;
       if (!updater) throw new Error('Asynchronous value setting is not supported without an updater function in settings.');
-      return updater(value)?.then(() => (this.#value = value, this)) as AsyncReturn<R, this>;
+      return updater(value)?.then(() => (super.setValue(value), this)) as AsyncReturn<S, this>;
     }
-    return (this.#value = value), this as AsyncReturn<R, this>;
+    return super.setValue(value);
   }
 }
