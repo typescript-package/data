abc8bc6e631c60f78eafc0ca0fef1676c92c047a
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-05-08 22:54:13 +0000

    refactor: enhance AdaptableData class with improved constructor overloads and type definitions

--- src/adaptable/lib/adaptable.data.class.ts
+++ src/adaptable/lib/adaptable.data.class.ts
@@ -8,22 +8,42 @@ import { InferAsync } from "@typedly/data";
  * @export
  * @class AdaptableData
  * @template {DataAdapterShape<T, S> | undefined} [A=undefined] 
- * @template [T=unknown] 
- * @template {boolean} [S=InferAsync<A>] 
- * @template {readonly any[]} [G=[]] 
- * @extends {AdaptableDataBase<A, T, S, G>}
+ * @template [T=unknown] The type of the data value.
+ * @template {boolean} [S=InferAsync<A>] Whether the data is asynchronous.
+ * @template {readonly any[]} [G=[]] Additional arguments for the adapter.
+ * @extends {AdaptableDataBase<A, T, S, G, DataAdapterConstructor<A, T, S, G>>}
  */
 export class AdaptableData<
   A extends DataAdapterShape<T, S> | undefined = undefined,
   T = unknown,
   S extends boolean = InferAsync<A>,
   G extends readonly any[] = []
-> extends AdaptableDataBase<A, T, S, G> {
-  constructor(
-    value?: T,
-    adapter?: DataAdapterConstructor<A, T, S, G>,
-    ...args: G
-  ) {
-    super(value, adapter, ...args);
+> extends AdaptableDataBase<A, T, S, G, DataAdapterConstructor<A, T, S, G>> {
+  /**
+   * Creates an instance of `AdaptableData`.
+   * @constructor
+   * @param {?T} [value] The initial value of the data.
+   * @param {?DataAdapterConstructor<A, T, S, G>} [adapter] The adapter constructor to use for this data instance.
+   * @param {...G} args The additional arguments for the adapter.
+   */
+  constructor(value?: T, adapter?: DataAdapterConstructor<A, T, S, G>, ...args: G)
+
+  /**
+   * Creates an instance of `AdaptableData`.
+   * @constructor
+   * @param {?T} [value] The initial value of the data.
+   * @param {...G} args The additional arguments for the adapter, if the first argument is not an adapter constructor.
+   */
+  constructor(value?: T, ...args: G)
+
+  /**
+   * Creates an instance of `AdaptableData`.
+   * @constructor
+   * @param {?T} [value] The initial value of the data.
+   * @param {?*} [adapterOrArg] The adapter constructor or the first additional argument.
+   * @param {...G} args The additional arguments for the adapter.
+   */
+  constructor(value?: T, adapterOrArg?: any, ...args: G) {
+    super(value, adapterOrArg, ...args);
   }
 }
